a2e1cbe15ddb82c2c7fc5e7a006b968ab20032f0
curryforthree <guyuliu0112@163.com>
2019-11-24 12:05:50 +0800

    数组/set

---
+++ 242.py
@@ -0,0 +1,16 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        res = True
+        a = set(s)
+        b = set(t)
+
+        if a == b:
+            for i in a:
+                res = res and s.count(i) == t.count(i)
+        else:
+            res = False
+        return res    
+
+
+x = Solution()
+print(x.isAnagram("cat", "rat"))
