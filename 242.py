286c4b4b344261d7c39f4a793f938850ec76d6e6
curryforthree <guyuliu0112@163.com>
2019-12-20 09:53:25 +0800

    数组/set

--- 242.py
+++ 242.py
@@ -3,7 +3,7 @@ class Solution:
         res = True
         a = set(s)
         b = set(t)
-
+        
         if a == b:
             for i in a:
                 res = res and s.count(i) == t.count(i)
