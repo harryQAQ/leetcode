e78ab4eb7cb8011e93025520d731cf9dfc23fc77
curryforthree <guyuliu0112@163.com>
2020-01-20 23:31:58 +0800

    heap,优先队列

---
+++ 347.py
@@ -0,0 +1,7 @@
+from collections import Counter
+from typing import List
+
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        return [i[0] for i in Counter(nums).most_common(k)]
+
