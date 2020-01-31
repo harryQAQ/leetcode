25a37745443c13d6210e4ee19024ceaca981aa38
curryforthree <guyuliu0112@163.com>
2020-01-31 19:37:20 +0800

    stack

--- 20.py
+++ 20.py
@@ -1,10 +1,10 @@
 class Solution:
     def isValid(self, s: str) -> bool:
         stack = []
-        mp = {')':'()', ']':'[', '}':'{'}
+        mp = {')':'(', ']':'[', '}':'{'}
         for i in s:
-            if mp.get(i, 0) == 0:
+            if mp.get(i, 0) == 0: #只要是左括号就压入栈中
                 stack.append(i)
-            elif (not stack) or stack.pop != mp[i]:
+            elif (not stack) or stack.pop != mp[i]: #右括号，则要匹配栈顶的左括号，若不匹配或者栈空则返回false
                 return False
         return not stack
