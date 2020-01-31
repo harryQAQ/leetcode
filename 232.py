4c27511826d9dc3b1f228e0b686268e0d9715ef5
curryforthree <guyuliu0112@163.com>
2020-01-31 19:37:27 +0800

    stack

---
+++ 232.py
@@ -0,0 +1,47 @@
+class MyQueue:
+
+    def __init__(self):
+        """
+        Initialize your data structure here.
+        """
+        self.instack = []  #输入栈
+        self.outstack = [] #输出栈
+
+    def push(self, x: int) -> None:
+        """
+        Push element x to the back of queue.
+        """
+        self.instack.append(x)
+
+        
+    def pop(self) -> int:
+        """
+        Removes the element from in front of queue and returns that element.
+        """
+        if not self.outstack:
+            while self.instack:
+                self.outstack.append(self.instack.pop()) 
+        return self.outstack.pop()
+
+    def peek(self) -> int:
+        """
+        Get the front element.
+        """
+        if not self.outstack:
+            while self.instack:
+                self.outstack.append(self.instack.pop()) 
+        return self.outstack[-1]
+
+    def empty(self) -> bool:
+        """
+        Returns whether the queue is empty.
+        """
+        return not self.instack and not self.outstack 
+
+
+# Your MyQueue object will be instantiated and called as such:
+# obj = MyQueue()
+# obj.push(x)
+# param_2 = obj.pop()
+# param_3 = obj.peek()
+# param_4 = obj.empty()
