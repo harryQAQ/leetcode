eba1b7421804ef05b2984999fb3a12b2d22e5d8c
curryforthree <guyuliu0112@163.com>
2020-02-11 09:57:29 +0800

    LCA

--- 235.py
+++ 235.py
@@ -7,8 +7,8 @@ class TreeNode:
 
 class Solution:
     def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
-        if not root or root == p or root == q: return root
         if q.val < root.val > p.val:
             return self.lowestCommonAncestor(root.left, p, q)
         if q.val > root.val < p.val:
             return self.lowestCommonAncestor(root.right, p, q)
+        return root
---
+++ 236.py
@@ -0,0 +1,16 @@
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+class Solution:
+    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+        if not root or root == p or root == q:
+            return root
+        l = self.lowestCommonAncestor(root.left, p, q)  
+        r = self.lowestCommonAncestor(root.right, p, q)
+        if l and r: return root
+        elif not l: return r      #返回的是右子树上的LCA，已解决子问题！！！
+        else: return l
