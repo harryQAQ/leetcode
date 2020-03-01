bc2a950ea40514f020ff4ba0740ffa532ba203d7
curryforthree <guyuliu0112@163.com>
2020-03-01 09:54:09 +0800

    动态规划

--- 121.py
+++ 121.py
@@ -3,11 +3,15 @@ from typing import List
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
         K = 1 #当前交易次数
+        n = len(prices)
+        if K >= n // 2:
+            return self.greedy(prices)
+
         MP = [[[0] * 2 for _ in range(K + 1)] for _ in range(len(prices))]
         res = []
-        # 设置初始状态, 无论什么k,第0天不持股一定是0收益，且不可能持股，可以处理成-price[0],或者-inf
+        # 设置初始状态,也就是第1天(i = 0),当k = 0时表示第1天不持股的状态一定是0收益，k = 1表示持股，-price[0]
         for i in range(K+1):
-            MP[0][i][0], MP[0][i][1] = 0, -float('inf')
+            MP[0][i][0], MP[0][i][1] = 0, -prices[0]
 
         for i in range(1, len(prices)):
             for k in range(K + 1):
@@ -18,7 +22,18 @@ class Solution:
                 else:
                     MP[i][k][0] = max(MP[i - 1][k][0], MP[i - 1][k - 1][1] + prices[i])
                 MP[i][k][1] = max(MP[i - 1][k][1], MP[i - 1][k][0] - prices[i])
-                res.append(MP[i][k][0])
+        for k in range(K + 1):
+            res.append(MP[n - 1][k][0])
+
         return max(res)
-                
+    
+
+    def greedy(self, prices):
+        res, n = 0, len(prices)
+        for i in range(1, n):
+            if prices[i - 1] < prices[i]:
+                res += prices[i] - prices[i - 1]
+        return res
+
+    
 
