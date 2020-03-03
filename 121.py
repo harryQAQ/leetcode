6693ddc7b870d0b8662c7ca8a1b12b035fbc28c1
curryforthree <guyuliu0112@163.com>
2020-03-03 10:02:34 +0800

    动态规划

--- 121.py
+++ 121.py
@@ -1,7 +1,7 @@
 from typing import List
-
 class Solution:
-    def maxProfit(self, prices: List[int]) -> int:
+    def maxProfit(self,prices: List[int]) -> int:
+        if not prices or not k: return 0
         K = 1 #最大交易次数
         n = len(prices)
         #如果K很大，状态数组会非常大，内存会爆，其实当K大于序列一半时，就可以退化为无数次交易的情况了。
@@ -10,19 +10,20 @@ class Solution:
 
         MP = [[[0] * 2 for _ in range(K + 1)] for _ in range(len(prices))]
         res = []
-        # 设置初始状态,也就是第1天(i = 0),当k = 0时表示第1天不持股的状态一定是0收益，k = 1表示持股，-price[0]
-        for i in range(K+1):
-            MP[0][i][0], MP[0][i][1] = 0, -prices[0]
+        #这里要假定买入股票算一次交易完成，那么在初始化的时候要对两个状态进行初始化
+        # 设置初始状态
+        MP[0][1][1] = -prices[0]
+        for k in range(2, K+1):
+            MP[0][k][1] = float('-inf')
+        #由于假定 买入就算交易次数，任何一天，0次交易是不可能持股的
+        for i in range(n):
+            MP[i][0][1] = float('-inf')
+
 
         for i in range(1, len(prices)):
-            for k in range(K + 1):
-                # if not k:#k = 0，一定不持有股票
-                #     MP[i][k][0] = MP[i]
-                if not k:
-                    MP[i][k][0] = MP[i - 1][k][0]
-                else:
-                    MP[i][k][0] = max(MP[i - 1][k][0], MP[i - 1][k - 1][1] + prices[i])
-                MP[i][k][1] = max(MP[i - 1][k][1], MP[i - 1][k][0] - prices[i])
+            for k in range(1, K + 1):
+                MP[i][k][0] = max(MP[i - 1][k][0], MP[i - 1][k][1] + prices[i]) 
+                MP[i][k][1] = max(MP[i - 1][k][1], MP[i - 1][k - 1][0] - prices[i]) #买入算一次交易
         for k in range(K + 1):
             res.append(MP[n - 1][k][0])
 
---
+++ 309.py
@@ -0,0 +1,14 @@
+from typing import List
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        
+        n = len(prices)
+        if n <= 1: return 0
+        MP = [[0] * 2 for _ in range(n + 1)]
+        MP[1][1], MP[0][0] = -prices[0], float('-inf') #初始化
+        prices.insert(0, 0)  # 下标1算第一天, 方便处理
+        for i in range(2, n + 1):
+            MP[i][0] = max(MP[i - 1][0], MP[i - 1][1] + prices[i])   #不动或卖掉
+            MP[i][1] = max(MP[i - 1][1], MP[i - 2][0] - prices[i])   #不动或买入
+
+        return MP[n][0]
