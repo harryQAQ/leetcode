cd5ae0c263c0c2a55e1aa11cb69aa88e2d7709c2
curryforthree <guyuliu0112@163.com>
2020-03-05 22:36:11 +0800

    模拟，数学

---
+++ 1103.py
@@ -0,0 +1,26 @@
+from typing import List
+class Solution:
+    def distributeCandies(self, candies: int, num_people: int) -> List[int]:
+        # n个小朋友
+        n = num_people
+        # 套用推出来的公式，直接计算可以完整发放糖果的次数p
+        p = int((2 * candies + 0.25)**0.5 - 0.5) 
+        # 算出完整发放糖果以后剩余的糖果数量
+        R = int(candies - (p + 1) * p * 0.5)
+
+        # 迭代rows轮，cols是倒霉孩子的下标
+        rows, cols = p // n, p % n
+
+        res = [0] * n
+        for i in range(rows):
+            res[i] = (i + 1) * rows + int(rows * (rows - 1) * 0.5) * n
+
+            # 最后一轮or在p<n时的第一轮
+            if i < cols:
+                res[i] += i + 1 + rows * n
+        #最后一个小孩拿
+        res[cols] += R
+        return res
+
+x = Solution()
+print(x.distributeCandies(7, 4))
