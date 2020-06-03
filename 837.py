37fddfdd30ff87aea000590087d2ab42a0f8ac28
curryforthree <guyuliu0112@163.com>
2020-06-03 22:01:22 +0800

    动态规划

---
+++ 837.py
@@ -0,0 +1,19 @@
+class Solution:
+    def new21Game(self, N: int, K: int, W: int) -> float:
+        dp = [None] * (K + W)
+        tmp = 0
+        for i in range(K, len(dp)):
+            dp[i] = 1 if i < N else 0
+            tmp += dp[i]
+        
+        for i in range(K - 1, -1, -1):
+            dp[i] = tmp / W
+            tmp = tmp + dp[i] - dp[i + W]
+        return dp[0]
+
+
+
+
+x = Solution()
+print(x.new21Game(10, 1, 10))
+
