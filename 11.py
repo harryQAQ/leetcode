dcf9f0e1f58eba640273e5aba51ad3a54c58f505
curryforthree <guyuliu0112@163.com>
2020-06-08 15:41:44 +0800

    双指针

--- 11.py
+++ 11.py
@@ -3,4 +3,11 @@ from typing import List
 class Solution:
     def maxArea(self, height: List[int]) -> int:
         if not height: return 0
-        
+        res, left, right= 0, 0, len(height) - 1 
+        while left < right:
+            res = max(res, (right - left)* min(height[left], height[right]))
+            if height[left] < height[right]:
+                left += 1
+            else:
+                right -= 1
+        return res
