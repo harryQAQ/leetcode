c9694c182635ba4587aef80727868841cd642cef
curryforthree <guyuliu0112@163.com>
2020-07-08 23:13:46 +0800

    动态规划

--- 322.py
+++ 322.py
@@ -7,5 +7,5 @@ class Solution:
         for i in range(1, amount + 1):
             for j in range(len(coins)):
                 if coins[j] <= i:
-                    dp[i] = min(dp[i], dp[i - coins[j]]) + 1
+                    dp[i] = min(dp[i], dp[i - coins[j]] + 1)
         return -1 if dp[amount] > amount else dp[amount]
