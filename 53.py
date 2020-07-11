44627e2266d9ff0ecc7b4d06d3db028075b7fedd
curryforthree <guyuliu0112@163.com>
2020-07-11 16:13:33 +0800

    dp

--- 53.py
+++ 53.py
@@ -4,8 +4,6 @@ class Solution:
         n = len(nums)
         dp = [0] * n
         dp[0] = nums[0]
-
         for i in range(1, n):
             dp[i] = max(nums[i], dp[i - 1] + nums[i])
-
         return max(dp)
