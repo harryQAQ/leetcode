b2c632b2924698edffc6a97b4c7dd665195b002b
curryforthree <guyuliu0112@163.com>
2020-07-11 16:13:04 +0800

    dp

---
+++ 53.py
@@ -0,0 +1,11 @@
+from typing import List
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
+        n = len(nums)
+        dp = [0] * n
+        dp[0] = nums[0]
+
+        for i in range(1, n):
+            dp[i] = max(nums[i], dp[i - 1] + nums[i])
+
+        return max(dp)
