68d80979173213e28d4763298656ffbeaa8873ba
curryforthree <guyuliu0112@163.com>
2020-08-19 17:15:42 +0800

    dp/记忆化回溯

---
+++ 139.py
@@ -0,0 +1,11 @@
+from typing import List
+class Solution:
+    def wordBreak(self, s: str, wordDict: List[str]) -> bool:
+        n = len(s)
+        dp = [False for _ in range(n + 1)]
+        dp[0] = True
+        for i in range(n):
+            for j in range(i + 1, n + 1):
+                if dp[i] and s[i:j] in wordDict:
+                    dp[j] = True 
+        return dp[-1]
