6875662afc71b73f94e6280a875d101ed391f5b2
curryforthree <guyuliu0112@163.com>
2020-08-19 18:29:21 +0800

    dp/记忆化回溯

--- 139.py
+++ 139.py
@@ -9,3 +9,19 @@ class Solution:
                 if dp[i] and s[i:j] in wordDict:
                     dp[j] = True 
         return dp[-1]
+
+
+# from functools import lru_cache
+
+# class Solution:
+#     def wordBreak(self, s: str, wordDict: List[str]) -> bool:
+#         @lru_cache(None)
+#         def back_track(s):
+#             if(not s):
+#                 return True
+#             res=False
+#             for i in range(1,len(s)+1):
+#                 if(s[:i] in wordDict):
+#                     res=back_track(s[i:]) or res
+#             return res
+#         return back_track(s)
