9f4376c60bd0f98150fb6f3c5b763dde6f11393e
curryforthree <guyuliu0112@163.com>
2020-08-30 12:03:42 +0800

    二叉树的后序遍历

---
+++ 145.py
@@ -0,0 +1,17 @@
+from typing import List
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+class Solution:
+    def postorderTraversal(self, root: TreeNode) -> List[int]:
+        res = []
+        def helper(root):
+            if not root: return
+            helper(root.left)
+            helper(root.right)
+            res.append(root.val)
+        helper(root)
+        return res 
